30258fe8936c2f0a9dce3e32aab6e69cb865735d
Ollah16 <olaoluwaboluwatife@yahoo.com>
2026-01-01 15:26:48 +0000

    refactor: remove userContext

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import './styles/darkmode.css';
 import './styles/lightmode.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { ThemeContext } from './context/themeContext';
-import { AboutMeProvider } from './context/userContext';
 import Layout from '../src/pages/layout'
 function App() {
 
   return (
     <ThemeContext>
-      <AboutMeProvider>
-        <Layout />
-      </AboutMeProvider>
+      <Layout />
     </ThemeContext >);
 }
 
