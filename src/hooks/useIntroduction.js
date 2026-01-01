22b9d504b8676fe95731657c6e62de0c19a03ba1
Ollah16 <olaoluwaboluwatife@yahoo.com>
2026-01-01 15:29:57 +0000

    refactor: add custom hook

---
+++ src/hooks/useIntroduction.js
@@ -0,0 +1,70 @@
+import { useEffect, useMemo, useState } from "react";
+import { trackEnter } from "../analytics/analytics";
+
+
+const FIRST_INTRO = `> self.learnAboutMe( );\nPress Enter to see what I am about.`;
+const TO_REPLACE = "Press Enter to see what I am about.";
+const LOADED_DATA = `Loaded data . . .`;
+const SECOND_INTRO = `
+        \n> self.currentLocation( );
+        ["Dallas, Texas"]
+        \n> self.education( );
+        ["MSc Engineering Management, 2022-2023", "B.Tech Agricultural Engineering, 2012-2020"]
+        \n> self.techStack( );
+        ["HTML", "CSS", "Javascript", "Typescript", "React.js", "Next.js", "Redux", "Jotai", "Tailwind", "Playwright", "Jest", "Node.js", "Git", "GitHub", "Vercel", "Firebase", "AWS", ...rest]
+        \n> self.contactMe( );
+        ["Github","Email"]`
+
+export const useIntroduction = () => {
+
+    const [introductionHeader, setIntroductionHeader] = useState('')
+    const [introductionBody, setIntroductionBody] = useState('')
+
+    useEffect(() => {
+        let timeOut, start = 0
+
+        const handleIntroduction = () => {
+            if (start < FIRST_INTRO.length) {
+                setIntroductionHeader(prev => prev + FIRST_INTRO[start])
+                timeOut = setTimeout(() => {
+                    start++
+                    handleIntroduction()
+                }, 50)
+            }
+        }
+
+        handleIntroduction()
+
+        return () => clearTimeout(timeOut)
+
+    }, [])
+
+    useEffect(() => {
+        let timeout;
+        const canEnter = introductionHeader.endsWith("about.");
+        const handleIntroduction = (event) => {
+            if (event?.key === "Enter" && canEnter) {
+                trackEnter()
+                setIntroductionHeader((prev) => prev.replace(TO_REPLACE, LOADED_DATA));
+                for (let i = 0; i < SECOND_INTRO.length; i++) {
+                    timeout = setTimeout(() => {
+                        setIntroductionBody((prev) => prev += SECOND_INTRO[i])
+                    }, i * 50)
+                }
+                document.removeEventListener('keypress', handleIntroduction)
+            }
+        }
+
+        document.addEventListener('keypress', handleIntroduction);
+
+        return () => {
+            document.removeEventListener('keypress', handleIntroduction);
+            clearTimeout(timeout)
+        }
+
+    }, [setIntroductionHeader, setIntroductionBody, introductionHeader]);
+
+    const contextValue = useMemo(() => ({ introductionBody, setIntroductionHeader, introductionHeader }), [introductionBody, setIntroductionHeader, introductionHeader]);
+
+    return contextValue
+}
