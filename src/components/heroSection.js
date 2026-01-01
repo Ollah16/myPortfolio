df8eeb0e07b69eabc4c7cacf26a6650ee451fda2
Ollah16 <olaoluwaboluwatife@yahoo.com>
2026-01-01 15:30:16 +0000

    refactor: extract hero section

---
+++ src/components/heroSection.js
@@ -0,0 +1,15 @@
+const HeroSection = () => {
+    return (
+        <section className={`hero-section`}>
+            <div className='hero-content'>
+                <div >
+                    <h2>Hi there, I'm Olaoluwa Oke!</h2>
+                    <p>Welcome to my portfolio website! I am a passionate Front End engineer with expertise in HTML, CSS, JavaScript, React and more. I love turning ideas into interactive and user-friendly websites. My commitment is to architect solutions that are not just functional, but intuitively beautiful.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HeroSection
