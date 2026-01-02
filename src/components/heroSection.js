7c1105f608eccb5a8c7bf84596424f2aff27bb8d
Ollah16 <olaoluwaboluwatife@yahoo.com>
2026-01-02 22:05:54 +0000

    style: use class styles

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -1,12 +1,10 @@
 const HeroSection = () => {
     return (
-        <section className={`hero-section`}>
-            <div className='hero-content'>
-                <div >
-                    <h2>Hi there, I'm Olaoluwa Oke!</h2>
-                    <p>Welcome to my portfolio website! I am a passionate Front End engineer with expertise in HTML, CSS, JavaScript, React and more. I love turning ideas into interactive and user-friendly websites. My commitment is to architect solutions that are not just functional, but intuitively beautiful.
-                    </p>
-                </div>
+        <section className='flex justify-center'>
+            <div className="text-center p-5 gap-4 md:w-2/4 w-3/4 flex flex-col">
+                <h2 className="dark:text-[#548a98] text-6xl font-bold">Hi, I'm Olaoluwa Oke!</h2>
+                <p className="font-light text-xl">Welcome to my portfolio website! I am a passionate Front End engineer with expertise in HTML, CSS, JavaScript, React and more. I love turning ideas into interactive and user-friendly websites. My commitment is to architect solutions that are not just functional, but intuitively beautiful.
+                </p>
             </div>
         </section>
     )
