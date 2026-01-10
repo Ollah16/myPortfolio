e518014449c8b053360d817ff2d4c3078716c474
Ollah16 <olaoluwaboluwatife@yahoo.com>
2026-01-10 18:03:55 +0000

    refactor: improve responsivenes

--- src/components/project/detail.js
+++ src/components/project/detail.js
@@ -9,7 +9,11 @@ const ProjectDetail = ({ project, index }) => {
     return (<section className={`lg:w-[50%] text-ellipsis overflow-hidden lg:h-[40%] rounded-none lg:rounded-xl ${index % 2 === 0 ? "lg:left-[-42%] lg:rounded-r-none" : "lg:right-[-42%] lg:rounded-l-none"} group dark:bg-[#030f1c] bg-[white] hover:bg-[#ffffffcc] text-black dark:hover:bg-[#030f1ccc] transition-all ease-in-out duration-150 dark:text-white p-4 lg:absolute relative rounded-none w-full flex h-fit flex-col text-ellipsis cursor-pointer gap-1`}>
 
         <div className='flex justify-between items-center'>
-            <h1 className='font-bold text-2xl lg:text-xl'>
+            <h1 className="
+                font-bold text-2xl lg:text-xl
+                truncate
+                w-[80%]
+                ">
                 {name}
             </h1>
             <Link className='no-underline' to={url}>
@@ -20,9 +24,14 @@ const ProjectDetail = ({ project, index }) => {
             <RiReactjsFill className="text-[#61DAFB]" />
             <span>{framework}</span>
         </div>
-        <div>
-            <p className='text-lg lg:text-sm font-light m-0 leading-normal'>{description}</p>
+        <div className="w-full">
+            <p className="text-lg lg:text-sm font-light m-0 leading-normal
+                   overflow-hidden lg:text-ellipsis
+                   lg:line-clamp-2 xl:line-clamp-none">
+                {description}
+            </p>
         </div>
+
     </section >)
 };
 
