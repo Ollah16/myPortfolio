a2881eb3d1499913ac29cf9fdeebcc4a0e174939
Ollah16 <olaoluwaboluwatife@yahoo.com>
2026-01-18 09:06:00 +0000

    .

--- src/components/NavBar/navBrand.js
+++ src/components/NavBar/navBrand.js
@@ -2,7 +2,7 @@
 const NavBrand = () => {
     return (
         <div>
-            <a href="/" className={`md:text-3xl py-3 text-2xl no-underline align-bottom inline-block font-bold uppercase tracking-widest dark:text-white text-black`}>Olaoluwa</a>
+            <a href="/" className={`md:text-3xl py-3 text-2xl no-underline align-bottom inline-block font-bold uppercase tracking-widest dark:text-white text-black`}>Damilola</a>
         </div>
     )
 }
