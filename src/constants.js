6dcafba0fbb610949331def1db8f74296477833a
Ollah16 <olaoluwaboluwatife@yahoo.com>
2026-01-19 14:22:54 +0000

    feat: comment unusued devDomain

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const devDomain = "http://localhost:3500";
+// const devDomain = "http://localhost:3500";
 const prdDomain = "https://portfolio-server-9e7m.vercel.app";
 
 export const eventTrackingAPI = `${prdDomain}/analytics/event`;
